9fcc5ff996dd88c0fb0cf5c42435e2e67ef9a03f
Marina Shvagir <alira_m@mail.ru>
2022-05-14 01:46:35 +0400

    Initial Commit

---
+++ New/NewApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewApp.swift
+//  New
+//
+//  Created by Марина Швагирь on 14.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct NewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
